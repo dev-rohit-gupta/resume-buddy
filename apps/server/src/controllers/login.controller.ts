9f4ba9dbb93dc85e26ae3f481924a745aacb9305
dev-rohit-gupta <dev.rohit.roxy@gmail.com>
2025-12-24 18:22:06 +0530

    refactor login controller to improve variable naming and formatting

--- apps/server/src/controllers/login.controller.ts
+++ apps/server/src/controllers/login.controller.ts
@@ -1,4 +1,4 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import { loginService } from "../services/login.service.js";
 import { asyncHandler } from "@resume-buddy/utils";
 import { ApiError } from "@resume-buddy/utils";
@@ -6,16 +6,15 @@ import { ApiResponse } from "@resume-buddy/utils";
 import { cookieOptions } from "../config/config.js";
 
 export const authController = asyncHandler(async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(400).json(new ApiError(400, "Email and password are required"));
-    }
-    // Call login service
-    const result = await loginService({ email, password });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json(new ApiError(400, "Email and password are required"));
+  }
+  // Call login service
+  const { accessToken, user } = await loginService({ email, password });
 
-    // Set access token in HTTP-only cookie
-    res.cookie("accessToken", result.accessToken, cookieOptions);
+  // Set access token in HTTP-only cookie
+  res.cookie("accessToken", accessToken, cookieOptions);
 
-
-    res.status(200).json(new ApiResponse({user: result.user}, "Login successful"));
+  res.status(200).json(new ApiResponse({ user }, "Login successful"));
 });
