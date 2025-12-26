a3439da59906ed2e25e020f3217de7d2caf8a3ad
dev-rohit-gupta <dev.rohit.roxy@gmail.com>
2025-12-26 19:49:14 +0530

    feat: enhance signup process to include resume upload and extraction

--- apps/server/src/controllers/signup.controller.ts
+++ apps/server/src/controllers/signup.controller.ts
@@ -3,15 +3,34 @@ import { signupService } from "../services/signup.service.js";
 import { asyncHandler } from "@resume-buddy/utils";
 import { ApiError } from "@resume-buddy/utils";
 import { ApiResponse } from "@resume-buddy/utils";
-import { cookieOptions } from "../config/config.js";
+import { cookieOptions } from "../config/cookie.config.js";
+import { uploadToCloudinary } from "../services/cloudinary.service.js";
+import { emailSchema } from "@resume-buddy/schemas";
+import { analyzeResume } from "@resume-buddy/ai-engine";
+import z from "zod";
+
+const signupSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: emailSchema,
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
 
 export const signupController = asyncHandler(async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-
-  if (!name || !email || !password) {
-    throw new ApiError(400, "Name, email, and password are required");
+  const parsedBody = signupSchema.safeParse(req.body);
+  if (!parsedBody.success) {
+    const errors = parsedBody.error.message;
+    throw new ApiError(400, `Invalid input: ${errors}`);
   }
-  const { accessToken, user } = await signupService({ name, email, password });
+  const { name, email, password } = parsedBody.data;
+
+  const resume = req.file;
+  if (!resume) {
+    throw new ApiError(400, "resume is required");
+  }
+
+ 
+  // parse the resume
+  const { accessToken, user } = await signupService({ name, email, password , resume });
   res.cookie("accessToken", accessToken, cookieOptions);
   res.status(201).json(new ApiResponse({ user }, "User registered successfully"));
 });
