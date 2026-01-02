d77df7ccc50b89b36b7202b3ae507d946a814d72
dev-rohit-gupta <dev.rohit.roxy@gmail.com>
2026-01-02 20:45:52 +0530

    feat: initialize Cloudinary service in the main entry point

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,14 @@
 import dotenv from "dotenv";
 // Load environment variables
 dotenv.config();
-
+import { initCloudinary } from "./services/cloudinary.service.js";
 import app from "./app.js";
 import { connectToDataBase } from "./db/connection.js";
 
 // Connect to the database
 connectToDataBase();
+// Initialize Cloudinary
+initCloudinary();
 
 const PORT = process.env.PORT!;
 
