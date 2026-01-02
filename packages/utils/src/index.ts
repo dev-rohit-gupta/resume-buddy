14b555e4b70b370680ab355bdc13f267270e80c1
dev-rohit-gupta <dev.rohit.roxy@gmail.com>
2026-01-02 21:49:53 +0530

    feat: add verifyAccessToken and getToken functions for JWT handling

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -6,3 +6,4 @@ export * from "./getResumeConfidenceScore.js";
 export * from "./ai/mapInputsToContents.js";
 export * from "./resumeParser.js";
 export * from "./ai/safeParseAIJson.js";
+export * from "./auth/verifyToken.js";
