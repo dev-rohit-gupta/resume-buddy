ed8acfcfd57b8a057d3bd221d70f84fffc0dec20
dev-rohit-gupta <dev.rohit.roxy@gmail.com>
2026-01-28 17:51:45 +0530

    refactor: remove Cloudinary configuration as part of migration to AWS S3

--- apps/server/src/config/cloudinary.config.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigOptions } from "cloudinary";
-
-export function getCloudinaryConfig(): ConfigOptions {
-  return {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
-    api_key: process.env.CLOUDINARY_API_KEY!,
-    api_secret: process.env.CLOUDINARY_API_SECRET!,
-  };
-}
